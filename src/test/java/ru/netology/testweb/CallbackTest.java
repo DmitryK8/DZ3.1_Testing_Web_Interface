3d22f00870e7aaa352427397277de07c9f0b55c1
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-16 01:07:45 +0500

    почти готово

--- src/test/java/ru/netology/testweb/CallbackTest.java
+++ src/test/java/ru/netology/testweb/CallbackTest.java
@@ -3,19 +3,18 @@ package ru.netology.testweb;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
+        SelenideElement form = $("body");
+        form.$("[data-test-id=name] input").setValue("Иванов Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        $(".alert-success").shouldHave(text("Ваша заявка успешно отправлена!"));
     }
 }
