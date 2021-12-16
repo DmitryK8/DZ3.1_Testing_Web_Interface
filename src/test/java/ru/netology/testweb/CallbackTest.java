12173ee4882021f7f0519c540466477b7c4189e6
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-16 20:50:01 +0500

    переделал ошибки :)

--- src/test/java/ru/netology/testweb/CallbackTest.java
+++ src/test/java/ru/netology/testweb/CallbackTest.java
@@ -1,5 +1,6 @@
 package ru.netology.testweb;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
@@ -7,14 +8,16 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
+    private Selenide div;
+
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("body");
-        form.$("[data-test-id=name] input").setValue("Иванов Василий");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Иванов-Деточкин Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $(".alert-success").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        div.$("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
